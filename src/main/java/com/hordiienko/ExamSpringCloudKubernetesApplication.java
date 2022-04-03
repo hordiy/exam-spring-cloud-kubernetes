d1bed658a55c86cf435ea487e54f0097bc2170ee
Vladyslav_Hordiienko <Vladyslav_Hordiienko@epam.com>
2022-04-03 11:19:25 +0300

    Initial commit

---
+++ src/main/java/com/hordiienko/ExamSpringCloudKubernetesApplication.java
@@ -0,0 +1,13 @@
+package com.hordiienko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamSpringCloudKubernetesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamSpringCloudKubernetesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hordiienko/ExamSpringCloudKubernetesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hordiienko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamSpringCloudKubernetesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
